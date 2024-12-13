8d2290cdc405172b8794c74fe5812907acc5d8bf
petr-fol <folmerpetr21@gmail.com>
2024-12-13 16:35:11 +0500

    + точка входа

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-from defs import from_dec, in_dec
-print("чтобы выйти из программы введите '0'\n")
-while True:
+if __name__ == "main":
+    from defs import from_dec, in_dec
 
-    a = int(input("в какой системе счисления ваше число?\n"))
-    if a == 0:
-        break
-    b = int(input("в какую систему счисления перевести?\n"))
-    if b == 0:
-        break
-    c = input("ваше число?\n")
-    if c == 0:
-        break
+    print("чтобы выйти из программы введите '0'\n")
+    while True:
 
-
-    num_notation = (2, 8, 10, 16)
-    dec = int
-    num = 0
-    if a <= 36 and b <= 36:
-        dec = c
-        if a != 10:
-            dec = in_dec(c, a)
-        num = from_dec(dec, b)
-    else:
-        print("вы ввели систему счисления > 36, могут быть ошибки в вычислениях изза ограничений алфавита")
-        answer = input("продолжить: Enter\n")
-        if answer is None:
+        a = int(input("в какой системе счисления ваше число?\n"))
+        if a == 0:
             break
-        else:
-            dec = in_dec(c, a)
-            num = from_dec(dec, b)
+        b = int(input("в какую систему счисления перевести?\n"))
+        if b == 0:
+            break
+        c = input("ваше число?\n")
+        if c == 0:
+            break
 
-    print(num)
+        num_notation = (2, 8, 10, 16)
+        dec = int
+        num = 0
+        if a <= 36 and b <= 36:
+            dec = c
+            if a != 10:
+                dec = in_dec(c, a)
+            num = from_dec(dec, b)
+        else:
+            print("вы ввели систему счисления > 36, могут быть ошибки в вычислениях изза ограниченного алфавита")
+            answer = input("продолжить: Enter\n")
+            if answer is None:
+                break
+            else:
+                dec = in_dec(c, a)
+                num = from_dec(dec, b)
+
+        print(num)
